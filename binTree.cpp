37dae9ea46927f6bade8006a6d8e2282cc6e1d26
Eason Lu <aelnosu@outlook.com>
2024-02-10 23:11:46 -0800

    NOT TESTED: Init project and complete basic BST implementation

---
+++ binTree.cpp
@@ -0,0 +1,8 @@
+//
+// Created by eason on 2/10/24.
+//
+
+#include "binTree.h"
+
+namespace binTree {
+} // binTree
---
+++ binTree.h
@@ -0,0 +1,16 @@
+//
+// Created by eason on 2/10/24.
+//
+
+#ifndef RANDP_BINTREE_H
+#define RANDP_BINTREE_H
+
+namespace binTree {
+
+    class binTree {
+
+    };
+
+} // binTree
+
+#endif //RANDP_BINTREE_H
